60f7f8f708788e7c5eaa4a7db197b7ac8d46223b
jmutt78 <jmcintosh@docusites.com>
2019-03-28 10:33:55 -0700

    config

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 "use strict";
 
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb+srv://justin:Ndz45MXVU5xxTVQ@myserver-hddae.mongodb.net/myDB?retryWrites=true";
+  process.env.DATABASE_URL ||
+  "mongodb+srv://justin:Ndz45MXVU5xxTVQ@myserver-hddae.mongodb.net/myDB?retryWrites=true";
 exports.myDB_DATABASE_URL =
-  process.env.myDB_DATABASE_URL || "mongodb+srv://justin:Ndz45MXVU5xxTVQ@myserver-hddae.mongodb.net/myDB?retryWrites=true";
+  process.env.myDB_DATABASE_URL ||
+  "mongodb+srv://justin:Ndz45MXVU5xxTVQ@myserver-hddae.mongodb.net/myDB?retryWrites=true";
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
