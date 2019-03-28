a953e137e1fb7005af30b9c563c527d926a4d97e
jmutt78 <jmcintosh@docusites.com>
2019-03-28 15:58:32 -0700

    -

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 "use strict";
 
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/journal";
+  process.env.DATABASE_URL || "mongodb://localhost/my-journal-app";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-journal";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-my-journal-app";
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
