e1897fd638f20f5505c02dba1df0333bb071ef32
jmutt78 <jmcintosh@docusites.com>
2019-03-28 13:39:36 -0700

    -

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 exports.DATABASE_URL =
   process.env.DATABASE_URL || "mongodb://localhost/journal";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/ournal";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-journal";
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
