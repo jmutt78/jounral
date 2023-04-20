d6f6cb0a524104ca6308d32e99f4630bd1d83698
jmutt78 <jmcintosh@docusites.com>
2023-04-20 09:11:12 -0700

    working to fix the server

--- server.js
+++ server.js
@@ -1,52 +1,49 @@
-"use strict";
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const { DATABASE_URL, PORT } = require("./config");
-const thankfulRouter = require("./thankful");
-const dailyRouter = require("./daily");
-const greatRouter = require("./great");
+const { DATABASE_URL, PORT } = require('./config');
+const thankfulRouter = require('./thankful');
+const dailyRouter = require('./daily');
+const greatRouter = require('./great');
 const app = express();
-const cors = require("cors");
-const passport = require("passport");
-const path = require("path");
-const { router: usersRouter } = require("./users");
-const { router: authRouter, localStrategy, jwtStrategy } = require("./auth");
+const cors = require('cors');
+const passport = require('passport');
+const path = require('path');
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(cors());
-app.use("/thankful", thankfulRouter);
-app.use("/goal", dailyRouter);
-app.use("/great", greatRouter);
+app.use('/thankful', thankfulRouter);
+app.use('/goal', dailyRouter);
+app.use('/great', greatRouter);
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
-app.use("/api/users/", usersRouter);
-app.use("/api/auth/", authRouter);
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const jwtAuth = passport.authenticate("jwt", {
-  session: false
+const jwtAuth = passport.authenticate('jwt', {
+  session: false,
 });
 
 let server;
@@ -55,7 +52,12 @@ function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(
       databaseUrl,
-      err => {
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      },
+      (err) => {
         if (err) {
           return reject(err);
         }
@@ -64,7 +66,7 @@ function runServer(databaseUrl, port = PORT) {
             console.log(`Your app is listening on port ${port}`);
             resolve();
           })
-          .on("error", err => {
+          .on('error', (err) => {
             mongoose.disconnect();
             reject(err);
           });
@@ -76,8 +78,8 @@ function runServer(databaseUrl, port = PORT) {
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      console.log("Closing server");
-      server.close(err => {
+      console.log('Closing server');
+      server.close((err) => {
         if (err) {
           return reject(err);
         }
@@ -88,7 +90,7 @@ function closeServer() {
 }
 
 if (require.main === module) {
-  runServer(DATABASE_URL).catch(err => console.error(err));
+  runServer(DATABASE_URL).catch((err) => console.error(err));
 }
 
 module.exports = { runServer, app, closeServer };
